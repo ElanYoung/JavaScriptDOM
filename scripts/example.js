24bc045d8427e5b868da98ccbe92422c948f0216
william <991658923@qq.com>
2020-10-27 17:00:00 +0800

    First project submission

---
+++ scripts/example.js
@@ -0,0 +1,32 @@
+// var height = "about 5'10\" tall";
+// alert(height);
+
+// var beatles = Array(4)
+// beatles[0] = "John";
+// beatles[1] = "Paul";
+// beatles[2] = "George";
+// beatles[3] = "Ringo";
+
+// var beatles = Array("John", "Paul", "George", "Ringo");
+
+// var years = [1940, 1941, 1942];
+
+// 关联数组 不推荐
+// var lennon = Array();
+// lennon["name"] = "John";
+// lennon["year"] = 1940;
+// lennon["living"] = false;
+
+// var lennon = Object();
+// lennon.name = "John";
+// lennon.year = 1940;
+// lennon.living = false;
+
+// var lennon = { name: "John", year: 1940, living: false };
+
+// var beatles = {};
+// beatles.vocalist = lennon;
+
+// var num = 7.561;
+// var num = Math.round(num);
+// alert(num)
