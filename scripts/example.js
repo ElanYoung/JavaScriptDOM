4a29e2664357d11ddd56f0a62c60e85689779690
william <991658923@qq.com>
2020-10-29 17:22:14 +0800

    更新相关代码

--- scripts/example.js
+++ scripts/example.js
@@ -30,3 +30,50 @@
 // var num = 7.561;
 // var num = Math.round(num);
 // alert(num)
+
+// 违背了"行为应该与表现分离"的原则
+function insertParagraph(text) {
+    var str = "<p>";
+    str += text;
+    str += "</p>";
+    document.write(str);
+}
+
+window.onload = function () {
+    var testdiv = document.getElementById("testdiv1");
+    testdiv.innerHTML = "<p>I inserted <em>this</em> content.</p>"
+}
+
+// 元素节点的nodeType属性值是1
+// 属性节点的nodeType属性值是2
+// 文本节点的nodeType属性值是3
+window.onload = function () {
+    // 创建一个p元素节点
+    var para = document.createElement("p");
+    var testdiv = document.getElementById("testdiv2");
+    // 把这个p元素节点追加到test.html文档中的那个p元素节点上
+    testdiv.appendChild(para);
+    // 创建一个文本节点
+    var txt = document.createTextNode("Hello World");
+    // 把这个文本节点追加到刚才创建的那个p元素节点上
+    para.appendChild(txt);
+    var info = "nodeName：";
+    info += para.nodeName;
+    info += " nodeType：";
+    info += para.nodeType;
+    console.log(info)
+}
+
+window.onload = function () {
+    var para = document.createElement("p");
+    var txt1 = document.createTextNode("This is ");
+    para.appendChild(txt1);
+    var emphasis = document.createElement("em");
+    var txt2 = document.createTextNode("my");
+    emphasis.appendChild(txt2);
+    para.appendChild(emphasis);
+    var txt3 = document.createTextNode(" content.");
+    para.appendChild(txt3);
+    var testdiv = document.getElementById("testdiv3");
+    testdiv.appendChild(para);
+}
--- scripts/showPic.js
+++ scripts/showPic.js
@@ -1,30 +1,3 @@
-function prepareGallery() {
-    if (!document.getElementsByTagName) return false;
-    if (!document.getElementById) return false;
-    if (!document.getElementById("imagegallery")) return false;
-    var gallery = document.getElementById("imagegallery");
-    var links = gallery.getElementsByTagName("a");
-    for (var i = 0; i < links.length; i++) {
-        links[i].onclick = function () {
-            return showPic(this) ? false : true;
-        }
-    }
-}
-
-function showPic(whichpic) {
-    if (!document.getElementById("placeholder")) return false;
-    var source = whichpic.getAttribute("href");
-    var placeholder = document.getElementById("placeholder");
-    if (placeholder.nodeName != "IMG") return false;
-    placeholder.setAttribute("src", source);
-    if (document.getElementById("description")) {
-        var text = whichpic.getAttribute("title") ? whichpic.getAttribute("title") : "";
-        var desrciption = document.getElementById("description");
-        desrciption.firstChild.nodeValue = text;
-    }
-    return true;
-}
-
 function addLoadEvent(func) {
     var oldonload = window.onload;
     if (typeof window.onload != 'function') {
@@ -37,6 +10,65 @@ function addLoadEvent(func) {
     }
 }
 
+function insertAfter(newElement, targetElement) {
+    var parent = targetElement.parentNode;
+    if (parent.lastChild == targetElement) {
+        parent.appendChild(newElement);
+    } else {
+        parent.insertBefore(newElement, targetElement.nextSibling);
+    }
+}
+
+function prepareGallery() {
+    if (!document.getElementsByTagName) return false;
+    if (!document.getElementById) return false;
+    if (!document.getElementById("imagegallery")) return false;
+    var gallery = document.getElementById("imagegallery");
+    var links = gallery.getElementsByTagName("a");
+    for (var i = 0; i < links.length; i++) {
+        links[i].onclick = function () {
+            return showPic(this);
+        }
+    }
+}
+
+function preparePlaceholder() {
+    if (!document.createElement) return false;
+    if (!document.createTextNode) return false;
+    if (!document.getElementById) return false;
+    if (!document.getElementById("imagegallery")) return false;
+    var placeholder = document.createElement("img");
+    placeholder.setAttribute("id", "placeholder");
+    placeholder.setAttribute("src", "images/placeholder.gif");
+    placeholder.setAttribute("alt", "my image gallery");
+    var description = document.createElement("p");
+    description.setAttribute("id", "description");
+    var desctest = document.createTextNode("Choose an image.");
+    description.appendChild(desctest);
+    // document.body.appendChild(placeholder);
+    // document.body.appendChild(description);
+    var gallery = document.getElementById("imagegallery");
+    // gallery.parentNode.insertBefore(placeholder, gallery);
+    // gallery.parentNode.insertBefore(description, gallery);
+    insertAfter(placeholder, gallery);
+    insertAfter(description, gallery);
+}
+
+function showPic(whichpic) {
+    if (!document.getElementById("placeholder")) return false;
+    var source = whichpic.getAttribute("href");
+    var placeholder = document.getElementById("placeholder");
+    if (placeholder.nodeName != "IMG") return false;
+    placeholder.setAttribute("src", source);
+    if (!document.getElementById("description")) return false;
+    var text = whichpic.getAttribute("title") ? whichpic.getAttribute("title") : "";
+    var desrciption = document.getElementById("description");
+    if (description.firstChild.nodeType == 3) {
+        description.firstChild.nodeValue = text;
+    }
+    return false;
+}
+
 function countBodyChildren() {
     var body_element = document.getElementsByTagName("body")[0];
     console.log(body_element.childNodes.length)
@@ -44,4 +76,5 @@ function countBodyChildren() {
 }
 
 addLoadEvent(prepareGallery)
+addLoadEvent(preparePlaceholder)
 addLoadEvent(countBodyChildren)
